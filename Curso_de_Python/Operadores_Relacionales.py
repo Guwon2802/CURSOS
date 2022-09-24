9641951157ca4cec424ca26752a0c762eba9af54
Guwon2802 <gabrielacuna@gmail.com>
2022-09-24 20:47:24 -0300

    #

---
+++ Curso_de_Python/Operadores_Relacionales.py
@@ -0,0 +1,41 @@
+# Se utilizan para establecer una relacion entre 2 valores.
+# Compara estos valores entre si y esta comparacion produce un resultado de verdadero o falso.
+# Tienen el mismo nivel de prioridad en su evaluacion. Si hay muchos en una ecuacion, los resuelve de izquierda a derecha.
+# Los operadores relacionales tiene menor prioridad que los aritmeticos.
+
+
+# MAYOR QUE
+
+resultado = 3 > 10
+print(resultado)
+
+
+# MENOR QUE
+
+resultado = 5 > 10
+print(resultado)
+
+
+# MAYOR O IGUAL QUE
+
+resultado = 1 >= 3
+print(resultado)
+
+
+# MENOR O IGUAL QUE
+
+resultado = 2 <= 1
+print(resultado)
+
+
+# DIFERENCIAL
+
+resultado = 6 != 6
+print(resultado)
+
+
+# IGUAL
+
+resultado = 8 == 9
+print(resultado)
+
