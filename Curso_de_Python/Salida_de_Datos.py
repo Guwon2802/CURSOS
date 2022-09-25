10dea7c6fca23c3cb52ea3aa5b2dd8f5b75b3596
Guwon2802 <gabrielacuna@gmail.com>
2022-09-25 16:46:47 -0300

    #

---
+++ Curso_de_Python/Salida_de_Datos.py
@@ -0,0 +1,7 @@
+# Sirve para hacer una salda de datos por consola mas facil.
+
+nombre = "Gabriel"
+edad = 18
+
+print("Mi nombre es {} y tengo {} años".format(nombre, edad)) # A "format" le agregas los valores con los que tendria que llenar los corchetes, obvio en orden.
+
