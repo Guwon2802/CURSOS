f46130df4eb8ac7d9ddc8f4ab73df37536a6f58c
Guwon2802 <gabrielacuna@gmail.com>
2022-09-25 16:49:20 -0300

    #

--- Curso_de_Python/Salida_de_Datos.py
+++ Curso_de_Python/Salida_de_Datos.py
@@ -5,3 +5,9 @@ edad = 18
 
 print("Mi nombre es {} y tengo {} años".format(nombre, edad)) # A "format" le agregas los valores con los que tendria que llenar los corchetes, obvio en orden.
 
+
+
+nombre = "Ezequiel"
+edad = 18
+
+print(f"Hola soy {nombre} y tengo {edad} años") # Esta es otra forma todavia mas facil.
