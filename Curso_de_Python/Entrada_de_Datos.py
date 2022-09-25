4d6ef2b8e280111d9526f153eba4e5532fbdcb0c
Guwon2802 <gabrielacuna@gmail.com>
2022-09-25 17:10:21 -0300

    #

--- Curso_de_Python/Entrada_de_Datos.py
+++ Curso_de_Python/Entrada_de_Datos.py
@@ -1,6 +1,8 @@
 # Sirve para pedirle un dato al usuario y que se guarde en una variable.
 
 nombre = input("Coloque aqui su nombre: ") # Con "input" se guarda un dato de tipo "str".
-                                           #
+# Si quieres agregar un mensajito, lo haces entre comillas.
+# El programa no continuara hasta que le coloques algo.
+# Tambien guarda espacios, como para agregar tu nombre completo. 
 
-print(f"Hola {nombre}")
+print(f"Hola {nombre}") # Asi lo estarias saludando.
