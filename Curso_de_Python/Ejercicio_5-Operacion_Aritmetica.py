64c9a0345f89875bd1c331a4542d7c4b04fb1742
Guwon2802 <gabrielacuna@gmail.com>
2022-09-27 01:05:53 -0300

    #

--- Curso_de_Python/Ejercicio_5-Operacion_Aritmetica.py
+++ Curso_de_Python/Ejercicio_5-Operacion_Aritmetica.py
@@ -1,8 +1,8 @@
 # EJERCICIO 5
 # Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber cuanto debera pagar finalmente por su compra.
 
-precio = float(print("Precio: "))
-porcentaje = 15 
+precio = float(input("Precio: "))
+descuento = precio * 0.15 # Asi sacas un porcentaje
 
-
-resultado = precio 
+precio_final= precio - descuento
+print(f"El precio final a pagar es de ${precio_final:.2f}")
