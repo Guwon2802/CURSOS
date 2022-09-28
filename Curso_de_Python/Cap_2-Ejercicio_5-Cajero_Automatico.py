31af3cb30c46a3760bafe28afef5f10b939df012
Guwon2802 <gabrielacuna@gmail.com>
2022-09-28 00:05:28 -0300

    #

---
+++ Curso_de_Python/Cap_2-Ejercicio_5-Cajero_Automatico.py
@@ -0,0 +1,11 @@
+# Capitulo 2: Condicionales
+
+'''
+EJERCICIO 5:
+Hacer un programa que simule un cajero automatico con un saldo inicial de $1000 y tendra el siguiente menu de opciones:
+1. Ingresar dinero en la cuenta
+2. Retirar dinero de la cuenta
+3. Mostrar dinero disponible
+4. Salir
+'''
+
