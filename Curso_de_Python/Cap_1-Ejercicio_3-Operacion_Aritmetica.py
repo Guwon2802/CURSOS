fa4db5ef9d597e9589afadf27da8184a63144e42
Guwon2802 <gabrielacuna@gmail.com>
2022-09-27 23:06:38 -0300

    #

---
+++ Curso_de_Python/Cap_1-Ejercicio_3-Operacion_Aritmetica.py
@@ -0,0 +1,14 @@
+# Capitulo 1: Elementos Basicos
+
+'''
+EJERCICIO 3:
+Hacer un programa para intercambiar el valor de 2 variables.
+'''
+
+a = (input("a -> "))
+b = (input("b -> "))
+
+a , b = b , a
+
+print(f"El nuevo valor de a es: {a}")
+print(f"El nuevo valor de b es: {b}")
