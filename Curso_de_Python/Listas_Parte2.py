abe80a1d24ece1d8e2c3239d126792e1a190992f
Guwon2802 <gabrielacuna@gmail.com>
2022-10-06 17:01:34 -0300

    #

--- Curso_de_Python/Listas_Parte2.py
+++ Curso_de_Python/Listas_Parte2.py
@@ -4,9 +4,31 @@ lista = ["Lunes","Martes","Miercoles","Jueves","Viernes",34,1.34,[1,2,3],True]
 
 print(len(lista)) #Te imprime la cantidad de elementos que hay en la lista.
 
+############################################################
 
 lista2 = [1,2,3,4,5]
 
-lista2.append(6) #Asi agregas elementos a tu lista, los agrega al final.
+lista2.append(6) #Agrega aun elemento a tu lista, lo agrega al final.
 
 print(lista2)
+
+############################################################
+
+lista3 = [1,2,4,5,6,7]
+
+lista3.insert(2,3) #Agrega un elemento a tu lista en la posicion que vos le indiques.
+                   #El primer parametro es en el indice donde lo queres agregar. El segundo es el valor que queres agregar.
+
+print(lista3)
+
+############################################################
+
+lista4 = [1,2,3,4]
+
+lista4.extend ([5,6,7,8])
+
+print(lista4)
+
+############################################################
+
+lista
