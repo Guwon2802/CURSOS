4d1a072c988ae3d9bc4f01edba1d0dc768dde222
Guwon2802 <gabrielacuna@gmail.com>
2022-10-17 16:50:04 -0300

    #

---
+++ Curso_de_Python/Cap_3-Ejercicio_1-Eliminar_Elementos_Duplicados_de_una_Lista.py
@@ -0,0 +1,9 @@
+#Capitulo 3: Colecciones
+
+'''
+EJERCICIO 1:
+Escriba un programa donde tenga una lista y que, a continuacion, elimine los elementos repetidos, por ultimo mostrar la lista.
+'''
+
+lista = [1,2,1,2,3,4,3,4,6,5,7,6,7,8,5,4,3,2,6,7,8]
+
