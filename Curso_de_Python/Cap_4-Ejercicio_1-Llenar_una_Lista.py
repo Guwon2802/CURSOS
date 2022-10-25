31fa1aa201af0adb318f36f83a3ac5f4803459dc
Guwon2802 <gabrielacuna@gmail.com>
2022-10-25 20:30:42 -0300

    #

--- Curso_de_Python/Cap_4-Ejercicio_1-Llenar_una_Lista.py
+++ Curso_de_Python/Cap_4-Ejercicio_1-Llenar_una_Lista.py
@@ -8,5 +8,28 @@ de la siguiente manera:
 1-2-3-4-5...-50
 '''
 
-#PROGRAMA
+#PROGRAMA 1
 
+lista = []
+
+#Agregamos a la lista los elementos del 1 al 50.
+i = 1
+while i<=50:
+    lista.append(i)
+    i+=1
+
+#Imprimiendo los elementos de la lista.
+for i in lista:
+    print(i,end="-")
+
+print() #Salto de linea
+
+
+#PROGRAMA 2
+
+#Agregamos a la lista los elementos del 1 al 50, de una manera mas facil.
+lista = list(range(1,51))
+
+#Imprimiendo los elementos de la lista.
+for i in lista:
+    print(i,end="-")
---
+++ Curso_de_Python/Cap_4-Ejercicio_2-Modificar_los_Elementos_de_una_Lista.py
@@ -0,0 +1,14 @@
+#Capitulo 4: Bucles
+
+'''
+EJERCICIO 2:
+Llenar una lista con los numeros del 1 al 10, luegos modificar los elementos de la lista multiplicandolos por un valor que el usuario digite.
+'''
+
+#PROGRAMA
+
+lista = list(range(1,11))
+print(f"Esta es la lista: {lista}")
+
+multiplicador = int(input("\nPor cuanto quieres multiplicarla: "))
+
