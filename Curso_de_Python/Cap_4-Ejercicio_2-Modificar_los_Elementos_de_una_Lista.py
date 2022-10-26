6d9c3b3f5d41100420b65fe77fff8aef7530c2c2
Guwon2802 <gabrielacuna@gmail.com>
2022-10-25 21:11:27 -0300

    #

--- Curso_de_Python/Cap_4-Ejercicio_2-Modificar_los_Elementos_de_una_Lista.py
+++ Curso_de_Python/Cap_4-Ejercicio_2-Modificar_los_Elementos_de_una_Lista.py
@@ -5,10 +5,43 @@ EJERCICIO 2:
 Llenar una lista con los numeros del 1 al 10, luegos modificar los elementos de la lista multiplicandolos por un valor que el usuario digite.
 '''
 
-#PROGRAMA
+#PROGRAMA 1
 
 lista = list(range(1,11))
-print(f"Esta es la lista: {lista}")
+print("Lista original: ")
+for i in lista:
+    print(i,end="-")
 
-multiplicador = int(input("\nPor cuanto quieres multiplicarla: "))
+valor = int(input("\nValor a multiplicar: "))
 
+#Multiplicar todos los elementos de la lista.
+indice = 0
+for i in lista:
+    lista[indice] *= valor
+    indice += 1
+
+
+print(f"\nLista final con los elementos multiplicados por {valor}")
+for i in lista:
+    print(i,end="-")
+
+print()
+print()
+
+#PROGRAMA 2
+
+lista = list(range(1,11))
+print("Lista original: ")
+for i in lista:
+    print(i,end="-")
+
+valor = int(input("\nValor a multiplicar: "))
+
+#Multiplicar todos los elementos de la lista.
+for indice,i in enumerate(lista): #Hace lo mismo que el primer programa, pero sin crear el indice y sin estar sumandole valores.
+    lista[indice] *= valor
+
+
+print(f"\nLista final con los elementos multiplicados por {valor}")
+for i in lista:
+    print(i,end="-")
---
+++ Curso_de_Python/Cap_4-Ejercicio_3-Insertar_Elementos_y_Ordenarlos.py
@@ -0,0 +1,6 @@
+#Capitulo 4: Bucles
+
+'''
+EJERCICIO 3:
+Pide numeros y metelos en una lista, cuando el usuario meta un 0 ya dejaremos de insertar. Por ultimo, muestra los numeros ordenados de menor a mayor.
+'''
