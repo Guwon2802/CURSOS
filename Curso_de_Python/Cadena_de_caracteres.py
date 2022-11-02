3e01667d841bd7f89a837674bce03cb41afa589f
Guwon2802 <gabrielacuna@gmail.com>
2022-11-02 14:40:55 -0300

    #

---
+++ Curso_de_Python/Cadena_de_caracteres.py
@@ -0,0 +1,34 @@
+cadena = "Hola mundo" #Esto es una cadena de texto.
+print(cadena)
+
+cadena = '"Estoy" "Estudiando"' #Poder usar comillas dobles, dentro de comillas simples.
+print(cadena)
+
+cadena = "Hola que \"tal?\"" #Pones delante "\" y no hara efecto.
+print(cadena)
+
+cadena = "Estoy \tdurmiendo" #Tabulacion.
+print(cadena)
+
+cadena = "Hola \nMundo" #Salto de linea.
+print(cadena)
+
+cadena = r"D:\nombre\trabajo" #Guarda en crudo toda la cadena.
+print(cadena)
+
+cadena = """Hola,
+Como
+Estas?
+""" #Cadena de varias lineas.
+print(cadena)
+
+print("""Hola
+Mundo
+Bello
+""")
+
+
+cadena1 = "Hola"
+cadena2 = " Mundo"
+
+print(cadena1 + cadena2) #Unir cadenas.
