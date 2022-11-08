f9ae05592288ffd304aedd59161773c6e88489a6
Guwon2802 <gabrielacuna@gmail.com>
2022-11-08 16:41:21 -0300

    #

--- Curso_de_Python/Comentarios.py
+++
@@ -1,9 +0,0 @@
-#Asi se hacen comentarios de una sola linea.
-
-'''
-    Asi se hacen 
-    comentarios 
-    multilineas.
-'''
-
-#Los comentarios no aparecen en el programa.
--- Curso_de_Python/Hola_Mundo.py
+++
@@ -1,3 +0,0 @@
-print("Hola Mundo\nMi nombre es Gabriel")
-
-#Con el "print" colocas algo en pantalla, si esta entre comillas mostrara la palabra o las palabras. Con el "\n" harias un salto de linea.
