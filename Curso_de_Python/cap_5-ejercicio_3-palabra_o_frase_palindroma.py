46aed8cbf70d429daa6f7b02a07be2a691ba4737
Guwon2802 <gabrielacuna@gmail.com>
2022-11-08 22:58:08 -0300

    #

--- Curso_de_Python/cap_5-ejercicio_3-palabra_o_frase_palindroma.py
+++ Curso_de_Python/cap_5-ejercicio_3-palabra_o_frase_palindroma.py
@@ -14,3 +14,15 @@ Ejemplos:
 
 cadena = input("Digite una cadena: ")
 
+#Primero, quitamos los espacios en blaco de la cadena
+cadena = cadena.replace(" ","")
+
+#Segundo, invertimos la cadena
+cadena2 = cadena[::-1]
+
+print(f"La cadena invertida es: {cadena2}")
+
+if cadena == cadena2:
+    print("La cadena es un palindromo")
+else:
+    print("La cadena NO es un palindromo")
