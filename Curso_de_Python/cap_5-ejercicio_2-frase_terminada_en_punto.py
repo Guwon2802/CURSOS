ff6bf8d60db999be874aceb2b436349086fb38cc
Guwon2802 <gabrielacuna@gmail.com>
2022-11-08 22:29:57 -0300

    #

---
+++ Curso_de_Python/cap_5-ejercicio_2-frase_terminada_en_punto.py
@@ -0,0 +1,17 @@
+#Capitulo 5: Cadenas
+
+'''
+Ejercicio 2:
+Hacer un programa para detectar si una frase introducida por el usuario finaliza con un "." o no. 
+Deberas imprimir por la consola una de las siguientes opciones; 
+"Termina con un punto" o por el contrario "No termina con un punto".
+'''
+
+#PROGRAMA
+
+cadena = input("Digite una frase: ")
+
+if cadena.endswith("."):
+    print("\nTermina con un punto")
+else:
+    print("\nNo termina con un punto")
---
+++ Curso_de_Python/cap_5-ejercicio_3-palabra_o_frase_palindroma.py
@@ -0,0 +1,16 @@
+#Capitulo 5: Cadenas
+
+'''
+Ejercicio 3:
+Hacer un programa que determine si una palabra o frase es palindroma.
+Una cadena palindroma se lee igual de izquierda a derecha que se derecha a izquierda.
+Ejemplos:
+    - oso
+    - reconocer
+    - anita lava la tina
+'''
+
+#PROGRAMA
+
+cadena = input("Digite una cadena: ")
+
