1566f757691115ad8b3fbb93d2fc7e88f1f9e935
Guwon2802 <gabrielacuna@gmail.com>
2023-01-27 17:22:05 -0300

    #

---
+++ Curso_de_Python/modulos.py
@@ -0,0 +1,11 @@
+'''
+Todos los archivos que terminan en extencion ".py" son modulos
+Podes crear modulos con funciones para reutilizar
+Desde un modulo llamas a otro modulo y podes usar todas sus funciones
+
+Hay tres tipos de modulos:
+    Los creados por python
+    Los creados por terceros(otra persona, una empresa, etc)
+    Los creados por ti
+'''
+
